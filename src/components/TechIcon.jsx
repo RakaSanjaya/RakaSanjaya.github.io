884fc4c84982b4a0e752310c38c5204fd460265c
Raka Sanjaya <sanjayaraka722@gmail.com>
2024-03-23 02:02:09 +0700

    add-dot-vite

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -10,7 +10,7 @@ export default function TechIcon({ icon, children, className }) {
       data-aos-duration="800"
       data-aos-easing="ease-out"
       class={`flex flex-col align-middle group duration-500 hover:saturate-100 ease-out hover:scale-105 z-[1] relative cursor-pointer`}>
-      <img src={`public/logo/${icon}-icon.png`} alt={icon} width="60px" className={`mx-auto ${className} duration-1000`} />
+      <img src={`/public/logo/${icon}-icon.png`} alt={icon} width="60px" className={`mx-auto ${className} duration-1000`} />
       <span class="text-center mx-auto text-xs relative font-medium mt-3 opacity-0 group-hover:opacity-100 bg-whiteDeep z-50 bg-opacity-40 shadow-md py-1 px-3 rounded-md delay-300 translate-y-4 group-hover:translate-y-0 duration-1000 ease-out dark:bg-greylighten dark:text-white">
         {children}
       </span>
